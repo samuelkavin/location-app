02462b9d3c61cb81e09410c4adebd81fb9bbcc2f
Samuel Kavin <samuelkavin.loganathan@cimb.com>
2021-06-08 10:40:38 +0800

    remove unused css code

--- src/components/material/MatCard.js
+++ src/components/material/MatCard.js
@@ -8,17 +8,6 @@ const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
 });
 
 const MatCard = (props) => {
